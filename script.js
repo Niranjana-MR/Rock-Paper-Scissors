15739b8163effc3f1b76e23c90856730c826a9f2
Niranjana M R <niranjan.mr99@gmail.com>
2021-06-03 21:43:58 +0530

    changed the logic. Cleaned up code.

--- script.js
+++ script.js
@@ -9,140 +9,163 @@ const subContainer = document.createElement('div');
 subContainer.classList.add('display');
 container.appendChild(subContainer);
 
-const roundNumber = document.createElement('h4');
-roundNumber.classList.add('display');
-subContainer.appendChild(roundNumber);
-
+//Choice the player made
 const choice = document.createElement('div');
 choice.classList.add('display');
 subContainer.appendChild(choice);
 
+//Each round result message
 const message = document.createElement('div');
 message.classList.add('display');
 subContainer.appendChild(message);
 
-const score = document.createElement('div');
-score.classList.add('display');
-subContainer.appendChild(score);
+//Player Score
+const pScore = document.createElement('div');
+pScore.classList.add('display');
+subContainer.appendChild(pScore);
 
-const finalResult = document.createElement('h4');
-finalResult.classList.add('display');
-subContainer.appendChild(finalResult);
+//Computer Score
+const cScore = document.createElement('div');
+cScore.classList.add('display');
+subContainer.appendChild(cScore);
 
-let playerScore = 0;
+//for displaying the winner
+const winner = document.createElement('h4');
+winner.classList.add('display');
+subContainer.appendChild(winner);
+
+let playerScore = 0,
+    computerScore = 0,
+    result = '';
+
 
 function game() {
-    let result = '';
-    let counter = 1;
 
-    btnRock.addEventListener('click', () => {
-        finalResult.textContent = '';
-        result = playRound('rock', computerPlay());
-        playerScore = calcScore(result, playerScore);
-        roundNumber.textContent = 'Round ' + counter;
-        choice.textContent = 'You played Rock...';
-        score.textContent = 'Your Score : ' + playerScore;
-        counter = checkCounter(counter);
-    });
+    btnRock.addEventListener('click', () => rock());
 
-    btnPaper.addEventListener('click', () => {
-        finalResult.textContent = '';
-        result = playRound('paper', computerPlay());
-        playerScore = calcScore(result, playerScore);
-        roundNumber.textContent = 'Round ' + counter;
-        choice.textContent = 'You played Paper...';
-        score.textContent = 'Your Score : ' + playerScore;
-        counter = checkCounter(counter);
-    });
+    btnPaper.addEventListener('click', () => paper());
 
-    btnScissors.addEventListener('click', () => {
-        finalResult.textContent = '';
-        result = playRound('scissors', computerPlay());
-        playerScore = calcScore(result, playerScore);
-        roundNumber.textContent = 'Round ' + counter;
-        choice.textContent = 'You played Scissors...';
-        score.textContent = 'Your Score : ' + playerScore;
-        counter = checkCounter(counter);
-    })
+    btnScissors.addEventListener('click', () => scissors());
 
-    btnClear.addEventListener('click', () => {
-        roundNumber.textContent = '';
-        choice.textContent = '';
-        finalResult.textContent = '';
-        result = '';
-        score.textContent = '';
-        counter = 1;
-        playerScore = 0;
-        message.textContent = '';
-    })
+    btnClear.addEventListener('click', () => clear());
 }
 
-function checkCounter(count) {
-    if (count < 5) {
-        count += 1;
-        return count;
-    } else {
-        showResult(playerScore);
-        return 1;
+
+function rock() {
+    checkWinner();
+    result = playRound('rock', computerPlay());
+    calcScore(result);
+    choice.textContent = 'You played Rock...';
+    pScore.textContent = 'Your Score : ' + playerScore;
+    cScore.textContent = 'Computer Score : ' + computerScore;
+    checkScore(playerScore, computerScore);
+}
+
+function paper() {
+    checkWinner();
+    result = playRound('paper', computerPlay());
+    calcScore(result);
+    choice.textContent = 'You played Paper...';
+    pScore.textContent = 'Your Score : ' + playerScore;
+    cScore.textContent = 'Computer Score : ' + computerScore;
+    checkScore(playerScore, computerScore);
+}
+
+function scissors() {
+    checkWinner();
+    result = playRound('scissors', computerPlay());
+    calcScore(result);
+    choice.textContent = 'You played Scissors...';
+    pScore.textContent = 'Your Score : ' + playerScore;
+    cScore.textContent = 'Computer Score : ' + computerScore;
+    checkScore(playerScore, computerScore);
+}
+
+function clear() {
+    choice.textContent = '';
+    pScore.textContent = '';
+    cScore.textContent = '';
+    winner.textContent = '';
+    message.textContent = '';
+    result = '';
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function checkScore(plrScr, comScr){
+    while(plrScr === 5 | comScr === 5){
+        showResult(plrScr, comScr);
+        break;
+    }
+    return;
+}
+
+function checkWinner(){
+    if(winner.textContent != ''){
+        clear();   
+    }
+    else{
+        return;
     }
 }
 
 //Display final result
-function showResult(finalScore) {
-    if (finalScore > 2) {
-        finalResult.textContent = 'Hurray... You win :)';
-        playerScore = 0;
-    } else {
-        finalResult.textContent = 'Oops... You Lose :('
-        playerScore = 0;
+function showResult(p, c) {
+    if(p>c){
+        winner.textContent = 'Hurray...! You Win :)';
+    }
+    else {
+        winner.textContent = 'ugh-oh...! You Loose :(';
     }
 }
 
 
 //Play a single round of rock-paper-scissors
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { //rock //randomPlay
 
     let res = '';
+
     playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
     //rock beats scissors
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            message.textContent = "It's a tie!";
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
+            message.textContent = 'Computer played Rock. It\'s a tie!';
             return res = 'tie';
-        } else if (computerSelection === "paper") {
-            message.textContent = "You Lose! Paper beats rock.";
-            return res = 'lose';
+        } else if (computerSelection === 'paper') {
+            message.textContent = 'Computer played Paper. You Lose!';
+            return res = 'cWin';
         } else {
-            message.textContent = "You Win! Rock beats scissors.";
-            return res = 'win';
+            message.textContent = 'Computer played Scissors. You Win!';
+            return res = 'pWin';
         }
     }
 
     //paper beats rock
-    if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            message.textContent = "You Win! Paper beats rock.";
-            return res = 'win';
-        } else if (computerSelection === "paper") {
-            message.textContent = "It's a tie!";
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            message.textContent = 'Computer played Rock. You win!';
+            return res = 'pWin';
+        } else if (computerSelection === 'paper') {
+            message.textContent = 'Computer played Paper. It\'s a tie!';
             return res = 'tie';
         } else {
-            message.textContent = "You Lose! Scissors beat paper.";
-            return res = 'lose';
+            message.textContent = 'Computer played Scissors. You Lose!';
+            return res = 'cWin';
         }
     }
 
     //scissors beats paper
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            message.textContent = "You Lose! Rock beats scissors.";
-            return res = 'lose';
-        } else if (computerSelection === "paper") {
-            message.textContent = "You Win! Scissors beats paper.";
-            return res = 'win';
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            message.textContent = 'Computer played Rock.\nYou Lose!';
+            return res = 'cWin';
+        } else if (computerSelection === 'paper') {
+            message.textContent = 'Computer played Paper.\nYou Win!';
+            return res = 'pWin';
         } else {
-            message.textContent = "It's a tie!";
+            message.textContent = 'Computer played Scissors.\nIt\'s a tie!';
             return res = 'tie';
         }
     }
@@ -152,21 +175,24 @@ function playRound(playerSelection, computerSelection) {
 function computerPlay() {
     let random = Math.round((Math.random() * 10));
     if (random <= 3) {
-        return ("rock");
+        return ('rock');
     } else if (random > 4 & random <= 7) {
-        return ("paper");
+        return ('paper');
     } else {
-        return ("scissors");
+        return ('scissors');
     }
 }
 
 //Calculates Player Score
-function calcScore(str, num) {
-    if (str === 'tie' | str === 'lose') {
-        return num;
-    } else if (str === 'win') {
-        return num + 1;
+function calcScore(score) {
+    if (score === 'tie') {
+        playerScore += 0;
+    } else if (score === 'cWin') {
+        computerScore += 1;
+    } else if (score === 'pWin') {
+        playerScore += 1;
     }
+
 }
 
 game();
